450d1bb194bb75a57a24aba3bcc90d7bbd3173bd
praseth002 <prasethsokha@gmail.com>
2025-11-13 23:54:24 +0700

    done i guess

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
-
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
@@ -45,26 +43,101 @@ import 'package:flutter/material.dart';
 //   );
 // }
 
+enum ButtonType { Primary, Secondary, Disabled }
 
+enum ButtonIconPos { Left, Right }
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text("Products"), centerTitle: false),
-      body: Container(
-        padding: EdgeInsets.all(50),
-        color: Colors.blue,
-        child: Column(
+// const ButtonColours = {
+//   ButtonType.Primary: Colors.green,
+//   ButtonType.Secondary: Colors.blue,
+//   ButtonType.Disabled: Colors.grey,
+// };
+extension ButtonTypeColour on ButtonType {
+  MaterialColor get color {
+    switch (this) {
+      case ButtonType.Primary:
+        return Colors.green;
+      case ButtonType.Secondary:
+        return Colors.blue;
+      case ButtonType.Disabled:
+        return Colors.grey;
+    }
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final ButtonType buttonType;
+  final String buttonText;
+  final IconData buttonIcon;
+  final ButtonIconPos iconPos;
+
+  const CustomButton({
+    super.key,
+    required this.buttonType,
+    required this.buttonText,
+    required this.buttonIcon,
+    this.iconPos = ButtonIconPos.Left,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        color: buttonType.color,
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ProductCard(product: Product.dart,),
-            ProductCard(product: Product.firebase,),
-            ProductCard(product: Product.flutter,),
+            if (iconPos == ButtonIconPos.Left) ...[
+              Icon(buttonIcon, color: buttonType.color.shade900),
+              SizedBox(width: 10),
+              Text(
+                buttonText,
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: buttonType.color.shade900,
+                ),
+              ),
+            ] else ...[
+              Text(
+                buttonText,
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: buttonType.color.shade900,
+                ),
+              ),
+              SizedBox(width: 10),
+              Icon(buttonIcon, color: buttonType.color.shade900),
+            ],
           ],
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       appBar: AppBar(title: Text("Products"), centerTitle: false),
+//       body: Container(
+//         padding: EdgeInsets.all(50),
+//         color: Colors.blue,
+//         child: Column(
+//           children: [
+//             ProductCard(product: Product.dart,),
+//             ProductCard(product: Product.firebase,),
+//             ProductCard(product: Product.flutter,),
+//           ],
+//         ),
+//       ),
+//     ),
+//   ));
+// }
 
 enum Product {
   dart(
@@ -96,10 +169,7 @@ enum Product {
 
 class ProductCard extends StatelessWidget {
   final Product product;
-  const ProductCard({
-    super.key,
-    required this.product
-  });
+  const ProductCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
@@ -112,73 +182,14 @@ class ProductCard extends StatelessWidget {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image(image: AssetImage(product.image), width: 75,),
-                SizedBox(height: 10,),
-                Text(product.title, style: TextStyle(fontSize: 25),),
-                SizedBox(height: 10,),
-                Text(product.description)
+                Image(image: AssetImage(product.image), width: 75),
+                SizedBox(height: 10),
+                Text(product.title, style: TextStyle(fontSize: 25)),
+                SizedBox(height: 10),
+                Text(product.description),
               ],
             ),
             // SizedBox(width: 5,)
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-enum ButtonType {Primary, Secondary, Disabled}
-enum ButtonIconPos {Left, Right}
-// const ButtonColours = {
-//   ButtonType.Primary: Colors.green,
-//   ButtonType.Secondary: Colors.blue,
-//   ButtonType.Disabled: Colors.grey,
-// };
-extension ButtonTypeColour on ButtonType {
-  MaterialColor get color{
-    switch (this) {
-      case ButtonType.Primary : return Colors.green;
-      case ButtonType.Secondary : return Colors.blue;
-      case ButtonType.Disabled : return Colors.grey;
-    }
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final ButtonType buttonType;
-  final String buttonText;
-  final IconData buttonIcon;
-  final ButtonIconPos iconPos;
-
-  const CustomButton({
-    super.key,
-    required this.buttonType,
-    required this.buttonText,
-    required this.buttonIcon,
-    this.iconPos = ButtonIconPos.Left});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      decoration: BoxDecoration(
-        color: buttonType.color,
-        borderRadius: BorderRadius.all(Radius.circular(50)),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (iconPos == ButtonIconPos.Left) ...[
-              Icon(buttonIcon, color: buttonType.color.shade900),
-              SizedBox(width: 10),
-              Text(buttonText, style: TextStyle(decoration: TextDecoration.none, color: buttonType.color.shade900))
-            ] else ... [
-              Text(buttonText, style: TextStyle(decoration: TextDecoration.none, color: buttonType.color.shade900)),
-              SizedBox(width: 10),
-              Icon(buttonIcon, color: buttonType.color.shade900),
-            ]
           ],
         ),
       ),
@@ -206,7 +217,7 @@ class HobbyCard extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
       child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20) ,
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
         child: Row(
           children: [
             hobbyIcon,
@@ -219,6 +230,176 @@ class HobbyCard extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Container(
+        color: const Color.fromARGB(255, 255, 247, 254),
+        child: Column(
+          children: [
+            CustomAppBar(),
+            Expanded(child: ListView(
+              children: [
+                WeatherCard(weatherIcon: 'assets/cloudy.png', city: 'Phnom Penh', minTemp: '10.0', maxTemp: '30.0', curTemp: '12.2', gradient: Colors.purple,),
+                WeatherCard(weatherIcon: 'assets/sunnyCloudy.png', city: 'Paris', minTemp: '10.0', maxTemp: '40.0', curTemp: '22.2', gradient: Colors.green,),
+                WeatherCard(weatherIcon: 'assets/sunny.png', city: 'Rome', minTemp: '10.0', maxTemp: '40.0', curTemp: '45.2', gradient: Colors.red,),
+                WeatherCard(weatherIcon: 'assets/veryCloudy.png', city: 'Toulouse', minTemp: '10.0', maxTemp: '40.0', curTemp: '45.2', gradient: Colors.orange,),
+                WeatherCard(weatherIcon: 'assets/cloudy.png', city: 'Phnom Penh', minTemp: '10.0', maxTemp: '30.0', curTemp: '12.2', gradient: Colors.purple,),
+                WeatherCard(weatherIcon: 'assets/sunnyCloudy.png', city: 'Paris', minTemp: '10.0', maxTemp: '40.0', curTemp: '22.2', gradient: Colors.green,),
+                WeatherCard(weatherIcon: 'assets/sunny.png', city: 'Rome', minTemp: '10.0', maxTemp: '40.0', curTemp: '45.2', gradient: Colors.red,),
+                WeatherCard(weatherIcon: 'assets/veryCloudy.png', city: 'Toulouse', minTemp: '10.0', maxTemp: '40.0', curTemp: '45.2', gradient: Colors.orange,),
+              ],
+            ))
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+class WeatherCard extends StatelessWidget {
+  final String weatherIcon;
+  final String city;
+  final String minTemp;
+  final String maxTemp;
+  final String curTemp;
+  final MaterialColor gradient;
+  const WeatherCard({
+    super.key, required this.weatherIcon, required this.city, required this.minTemp, required this.maxTemp, required this.curTemp, required this.gradient,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          PhysicalModel(
+            color: Colors.transparent,
+            elevation: 10,
+            shadowColor: Colors.black,
+            borderRadius: BorderRadius.all(Radius.circular(25)),
+            clipBehavior: Clip.antiAlias,
+            child: Container(
+              padding: EdgeInsetsGeometry.all(10),
+              decoration: BoxDecoration(
+                // borderRadius: BorderRadius.all(Radius.circular(25)),
+                gradient: LinearGradient(
+                  colors: [
+                    gradient.shade400,
+                    gradient.shade800,
+                  ],
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(
+                            weatherIcon,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            city,
+                            style: TextStyle(
+                              decoration: TextDecoration.none,
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                          Text(
+                            'Min: $minTemp°C',
+                            style: TextStyle(
+                              decoration: TextDecoration.none,
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          ),
+                          Text(
+                            'Max: $maxTemp°C',
+                            style: TextStyle(
+                              decoration: TextDecoration.none,
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Container(color: Colors.transparent),
+                      ),
+                      Text(
+                        "$curTemp°C",
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      color: const Color.fromARGB(255, 150, 200, 240),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // Expanded(child: Container()),
+              Container(
+                width: 50,
+                height: 5,
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
+              Container(
+                width: 50,
+                height: 5,
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
+              Container(
+                width: 50,
+                height: 5,
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 5),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
